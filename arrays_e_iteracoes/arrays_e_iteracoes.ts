2b3d108e7787f853b9b74186397f190ab81f903a
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-03-25 01:48:39 -0300

    Arrays e Iterações

---
+++ arrays_e_iteracoes/arrays_e_iteracoes.ts
@@ -0,0 +1,11 @@
+let notasAlunos: number[] = [10, 8, 8.5, 7];
+
+//índices - in(exibe o índice do array)
+for(let i in notasAlunos) {
+    console.log(i);
+}
+
+//valores - of(exibe os valores do array)
+for(let notaAluno of notasAlunos) {
+    console.log(notaAluno);
+}
